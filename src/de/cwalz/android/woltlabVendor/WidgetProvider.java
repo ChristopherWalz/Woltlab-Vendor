1bf49d11d325b17a3a1af37cc08b9d0317b1f43e
Christopher Walz <chris@cwalz.de>
2014-11-12 18:12:03 +0100

    "..." problem (hopefully) fixed

--- src/de/cwalz/android/woltlabVendor/WidgetProvider.java
+++ src/de/cwalz/android/woltlabVendor/WidgetProvider.java
@@ -13,70 +13,76 @@ import android.util.Log;
 import android.widget.RemoteViews;
 
 public class WidgetProvider extends AppWidgetProvider {
-    public static final String ACTION_UPDATE_CLICK = "de.cwalz.android.woltlabVendor.UPDATE_CLICK";	
-    public static final String PREFS_NAME = "de.cwalz.android.woltlabVendor.PREFS";
-    public static final String LOG_TAG = "de.cwalz.android.woltlabVendor.LOG_TAG";
-    public static final String LOADING_STRING = "...";
-    public static final int ALARM_ID = 6969;
-    
-    public void onUpdate(final Context context, final AppWidgetManager appWidgetManager, final int[] appWidgetIds) {
-    	super.onUpdate(context, appWidgetManager, appWidgetIds);
-    	if (!NetworkUtil.hasInternetConnection(context)) {
-    		return;
-    	}
-        updateAppWidget(context, appWidgetManager, appWidgetIds);
-    }
-    
-    public static void updateAppWidget(final Context context, AppWidgetManager appWidgetManager, final int[] appWidgetIds) {
-    	for (int appWidgetID : appWidgetIds) {
-	        // Create an Intent to launch ExampleActivity
-	        Intent intent = new Intent(context, MainActivity.class);
-	        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-	
-	        // Get the layout for the App Widget and attach an on-click listener
-	        // to the button
-	        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
-	        views.setOnClickPendingIntent(R.id.layoutContainer, pendingIntent);
-	
-	        // Tell the AppWidgetManager to perform an update on the current app widget
-	        appWidgetManager.updateAppWidget(appWidgetID, views);
-        }
-    	
-        SharedPreferences settings = context.getSharedPreferences(WidgetProvider.PREFS_NAME, 0);        
-        final int vendorID = settings.getInt("vendorID", 0);
-        final String apiKey = settings.getString("apiKey", "");
-        
-        // return false if configuration is not done yet
-        if (vendorID == 0 || apiKey.isEmpty()) {
-        	Log.i(WidgetProvider.LOG_TAG, "Configuration not done yet, return");
-        	return;
-        }        
-        
-        // update widget
-        TransactionsUtil.updateBalance(LOADING_STRING, context);
-        TransactionsUtil.update(context, new ICallback() {
-        	public void onSuccess(float newBalance) {
-        		if (newBalance != 0) {
-        			TransactionsUtil.updateBalance(String.valueOf(newBalance), context);
-        		}
-        		else {
-        			SharedPreferences settings = context.getSharedPreferences(WidgetProvider.PREFS_NAME, 0);
-        		    float balance = settings.getFloat("balance", 0.f);
-        			TransactionsUtil.updateBalance(String.valueOf(balance), context);
-        		}
-        	}
-			public void onFailure(String error) {}
-        });
-    }
-    
-    public static void forceWidgetUpdate(Context context) {
-    	AppWidgetManager man = AppWidgetManager.getInstance(context);
-    	int[] appWidgetIds = man.getAppWidgetIds(new ComponentName(context, WidgetProvider.class));
+	public static final String ACTION_UPDATE_CLICK = "de.cwalz.android.woltlabVendor.UPDATE_CLICK";
+	public static final String PREFS_NAME = "de.cwalz.android.woltlabVendor.PREFS";
+	public static final String LOG_TAG = "de.cwalz.android.woltlabVendor.LOG_TAG";
+	public static final String LOADING_STRING = "...";
+	public static final int ALARM_ID = 6969;
 
-		context.sendBroadcast(new Intent(AppWidgetManager.ACTION_APPWIDGET_UPDATE)
-			.setPackage(context.getPackageName())
-			.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, 
-				appWidgetIds
-			));
+	public void onUpdate(final Context context, final AppWidgetManager appWidgetManager, final int[] appWidgetIds) {
+		super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+		updateAppWidget(context, appWidgetManager, appWidgetIds);
+	}
+
+	public static void updateAppWidget(final Context context, AppWidgetManager appWidgetManager,
+			final int[] appWidgetIds) {
+
+		if (!NetworkUtil.hasInternetConnection(context)) {
+			return;
+		}
+
+		for (int appWidgetID : appWidgetIds) {
+			// Create an Intent to launch ExampleActivity
+			Intent intent = new Intent(context, MainActivity.class);
+			PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+			// Get the layout for the App Widget and attach an on-click listener
+			// to the button
+			RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+			views.setOnClickPendingIntent(R.id.layoutContainer, pendingIntent);
+
+			// Tell the AppWidgetManager to perform an update on the current app
+			// widget
+			appWidgetManager.updateAppWidget(appWidgetID, views);
+		}
+
+		SharedPreferences settings = context.getSharedPreferences(WidgetProvider.PREFS_NAME, 0);
+		final int vendorID = settings.getInt("vendorID", 0);
+		final String apiKey = settings.getString("apiKey", "");
+
+		// return false if configuration is not done yet
+		if (vendorID == 0 || apiKey.isEmpty()) {
+			Log.i(WidgetProvider.LOG_TAG, "Configuration not done yet, return");
+			return;
+		}
+
+		// update widget
+		TransactionsUtil.updateBalance(LOADING_STRING, context);
+		TransactionsUtil.update(context, new ICallback() {
+			public void onSuccess(float newBalance) {
+				if (newBalance != 0) {
+					TransactionsUtil.updateBalance(String.valueOf(newBalance), context);
+				} else {
+					SharedPreferences settings = context.getSharedPreferences(WidgetProvider.PREFS_NAME, 0);
+					float balance = settings.getFloat("balance", 0.f);
+					TransactionsUtil.updateBalance(String.valueOf(balance), context);
+				}
+			}
+
+			public void onFailure(String error) {
+				SharedPreferences settings = context.getSharedPreferences(WidgetProvider.PREFS_NAME, 0);
+				float balance = settings.getFloat("balance", 0.f);
+				TransactionsUtil.updateBalance(String.valueOf(balance), context);
+			}
+		});
+	}
+
+	public static void forceWidgetUpdate(Context context) {
+		AppWidgetManager man = AppWidgetManager.getInstance(context);
+		int[] appWidgetIds = man.getAppWidgetIds(new ComponentName(context, WidgetProvider.class));
+
+		context.sendBroadcast(new Intent(AppWidgetManager.ACTION_APPWIDGET_UPDATE).setPackage(context.getPackageName())
+				.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds));
 	}
 }
